cad1db81663355755e7f3a6b56ffa4664d719ed8
georgio <gardigo2001@gmail.com>
2023-07-10 23:48:48 -0300

    primeirocommit

---
+++ src/app/infocontent/infocontent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infocontent',
+  templateUrl: './infocontent.component.html',
+  styleUrls: ['./infocontent.component.scss']
+})
+export class InfocontentComponent {
+
+}
